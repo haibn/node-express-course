82e6352c8c913b1c6500c39928eb065d47da2199
haibn <haibinyuy@berkeley.edu>
2025-09-06 15:08:49 -0700

    week8 jwt work finished

--- 05-JWT-Basics/preferred/controllers/main-controllers.js
+++ 05-JWT-Basics/preferred/controllers/main-controllers.js
@@ -2,37 +2,20 @@
 const jwt = require("jsonwebtoken")
 
 const login = async (req, res) => {
-    const {username, password} = req.body
+    const {name, password} = req.body
 
-    if (!username || !password) {
-        res.status(400).json({msg: "Please provide a username and password"})
+    if (!name || !password) {
+        res.status(400).json({msg: "Please provide a name and password"})
     }
 
-    const id = new Date().getDate()
     const secret = process.env.JWT_SECRET
-    const token = jwt.sign({id, username}, secret, {expiresIn: "30d", })
+    const token = jwt.sign({ name }, secret, {expiresIn: "24h", })
 
-    res.status(200).json({token, msg: "User created"})
+    res.status(200).json({token: token})
 }
 
 const message = async (req, res) => {
-    const headerAuth = req.headers.authorization
-    console.log(headerAuth)
-
-    if (!headerAuth || !headerAuth.startsWith("Bearer ")) {
-        res.status(401).json({msg: "No token provided"})
-    }
-
-    console.log(headerAuth)
-
-    const token = headerAuth.split(" ")[1]
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        res.status(200).json(decoded)
-    } catch (error) {
-        res.status(401).json({msg: "Not valid token"})
-    }
+    res.status(200).json({message: `Hello ${req.user.name}!`})
 }
 
 module.exports = {
